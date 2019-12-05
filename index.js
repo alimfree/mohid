e2de1f011cb889dc6b756fc68e38de2c61a81c0f
Mohammed Ali <mali@teladoc.com>
2019-12-05 09:04:23 -0600

    clean up

--- index.js
+++
@@ -1,6 +0,0 @@
-const siteUrl = "https://us.mohid.co/tx/dallas/iccltx/masjid/widget/api/index/?m=prayertimings&wmode=opaque";
-const axios = require("axios");
-const fetchData = async () => {
-    const result = await axios.get(siteUrl);
-    return cheerio.load(result.data);
-};
